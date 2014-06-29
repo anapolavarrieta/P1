1fd4e66534d011c5075e83a05a31e351d3e94f4e
Ana Paula Olavarrieta <anapolavarrieta@gmail.com>
2014-06-29 10:07:07 -0400

    Made the first changes to the page, added an Image and added Bootstrap

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>P1 Ana Paula</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSS -->
+    <link href="Bootstrap/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+	<div class="container">
+		<div class="row">
+			<div class= "col-md-8">
+			<h3> In 2+ sentences describe who you are and what you do </h3>
+			<p>....</p>
+			<h3> Why are you taking this course?</h3>
+			<p>....</p>
+			<h3>What previous experience do you have with HTML/CSS?</h3>
+			<p>...</p>
+			<h3>Have you worked with PHP before? If so, to what extent?</h3>
+			<p>...</p>
+			<h3>Are you working on a Mac,PC or other platform? </h3>
+			<p>...</p>
+			<h3>Anything else we should know </h3>
+			<p>...</p>
+			</div>
+			<div class="col-md-4">
+			<p> <img src="Images/Ana_Paula.jpg" alt= "Ana Paula Photo" /> </p>
+			</div>
+		</div>
+	</div>
+	
+</body>
