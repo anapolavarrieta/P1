5e26427e1fe348f2d7825f3795546ef47f3909b7
Ana Paula Olavarrieta <anapolavarrieta@gmail.com>
2014-06-30 10:52:34 -0400

    Answeres the main questions about myself

--- index.php
+++ index.php
@@ -15,17 +15,20 @@
 		<div class="row">
 			<div class= "col-md-8">
 			<h3> In 2+ sentences describe who you are and what you do </h3>
-			<p>....</p>
+			<p>I am from Mexico, and I moved to Boston since January. I am currently doing home office for a travel agency located in Mexico.
+			<br/> As well, I am taking the Web Technologies certificate at the Harvard Extension School. This certificate is my first experience with
+			<br/> web development.</p>
 			<h3> Why are you taking this course?</h3>
-			<p>....</p>
+			<p>I am taking this course as part of the Web Technologies certificate. I am interested in this certificate because I want to learn
+			<br/> more about web development, in order for me to be able to start my own business</p>
 			<h3>What previous experience do you have with HTML/CSS?</h3>
-			<p>...</p>
+			<p>My first experience and only with HTML/CSS was in the previous course Fundamentals of Website Development</p>
 			<h3>Have you worked with PHP before? If so, to what extent?</h3>
-			<p>...</p>
+			<p>I have work a little bit with PHP in last course, but really basic. I am eager to learn much more bout it</p>
 			<h3>Are you working on a Mac,PC or other platform? </h3>
-			<p>...</p>
+			<p>I am working on a PC</p>
 			<h3>Anything else we should know </h3>
-			<p>...</p>
+			<p>One of my dreams would be to work with a Non Profit Organization helping in the web development of the organization.</p>
 			</div>
 			<div class="col-md-4">
 			<p> <img src="Images/Ana_Paula.jpg" alt= "Ana Paula Photo" /> </p>
